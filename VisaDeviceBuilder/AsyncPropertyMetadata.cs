50c0834ac279a517709103a7d6f96caaa0519890
Maxim Yudin <stibiu@yandex.ru>
2020-12-06 20:43:17 +0300

    Converted the metadata classes into C# records.
    The asynchronous property and device action metadata classes only store
    the read-only data, so they can be represented using the new C# 9 record
    objects.
    Also updated the NuGet packages.

--- VisaDeviceBuilder/AsyncPropertyMetadata.cs
+++ VisaDeviceBuilder/AsyncPropertyMetadata.cs
@@ -2,26 +2,25 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace VisaDeviceBuilder
 {
-  // TODO: Switch from "class" to "record" and from "set" to "init" when C# 9 support will be present in the IDE.
   /// <summary>
   ///   Defines the record for storing asynchronous property metadata.
   /// </summary>
   [ExcludeFromCodeCoverage]
-  public class AsyncPropertyMetadata
+  public record AsyncPropertyMetadata
   {
     /// <summary>
     ///   Gets the original name for the asynchronous property defined in the owning class.
     /// </summary>
-    public string OriginalName { get; set; } = string.Empty;
+    public string OriginalName { get; init; } = string.Empty;
 
     /// <summary>
     ///   Gets the user-friendly localized name for the asynchronous property.
     /// </summary>
-    public string LocalizedName { get; set; } = string.Empty;
+    public string LocalizedName { get; init; } = string.Empty;
 
     /// <summary>
     ///   Gets the actual asynchronous property instance.
     /// </summary>
-    public IAsyncProperty? AsyncProperty { get; set; }
+    public IAsyncProperty AsyncProperty { get; init; } = new AsyncProperty(() => string.Empty);
   }
 }
--- VisaDeviceBuilder/DeviceActionMetadata.cs
+++ VisaDeviceBuilder/DeviceActionMetadata.cs
@@ -3,26 +3,25 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace VisaDeviceBuilder
 {
-  // TODO: Switch from "class" to "record" and from "set" to "init" when C# 9 support will be present in the IDE.
   /// <summary>
   ///   Defines the record for storing device action metadata.
   /// </summary>
   [ExcludeFromCodeCoverage]
-  public class DeviceActionMetadata
+  public record DeviceActionMetadata
   {
     /// <summary>
     ///   Gets the original name for the device action defined in the owning class.
     /// </summary>
-    public string OriginalName { get; set; } = string.Empty;
+    public string OriginalName { get; init; } = string.Empty;
 
     /// <summary>
     ///   Gets the user-friendly localized name for the device action.
     /// </summary>
-    public string LocalizedName { get; set; } = string.Empty;
+    public string LocalizedName { get; init; } = string.Empty;
 
     /// <summary>
     ///   Gets the actual device action delegate.
     /// </summary>
-    public Action? DeviceAction { get; set; }
+    public Action DeviceAction { get; init; } = () => { };
   }
 }
